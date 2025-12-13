35072795efc723a3ad3384354352d1775199d5d8
CodWiz <codwiz@yandex.ru>
2025-12-13 03:30:38 +0300

    fix(i18n/ru): correction of spelling and style of Russian translation

--- locales/ru.js
+++ locales/ru.js
@@ -9,10 +9,10 @@ const ru = {
     "enableShortcutsText": "Включить/Отключить ярлыки",
     "editShortcutsText": "Редактировать ярлыки",
     "shortcutsInfoText": "Выберите, какие ярлыки будут отображаться",
-    "editShortcutsList": "Сохранёные ярлыки",
-    'editShortcutsListInfo': 'Вы можете добавить новые ярлыки нажав на "+" или отредактировать существующие, щёлкнув на ярлык или URL.',
-    "adaptiveIconText": "Адаптивные формы значков",
-    "adaptiveIconInfoText": "Ярлыки всегда будут круглыми",
+    "editShortcutsList": "Сохранённые ярлыки",
+    'editShortcutsListInfo': 'Вы можете добавить новые ярлыки, нажав на "+", или отредактировать существующие, щёлкнув по ярлыку или URL.',
+    "adaptiveIconText": "Адаптивные значки",
+    "adaptiveIconInfoText": "Значки всегда будут круглыми",
     "bookmarksText": "Закладки",
     "bookmarksInfo": "Показать боковую панель закладок",
     "ai_tools_button": "Инструменты ИИ",
@@ -21,12 +21,12 @@ const ru = {
     "aiToolsSettingsInfo": "Управление ярлыками ИИ",
     "googleAppsMenuText": "Приложения Google",
     "googleAppsMenuInfo": "Показать ярлыки для приложений Google",
-    "googleAppsHover": "Гугл приложения",
+    "googleAppsHover": "Приложения Google",
 
     // To-do List
-    "todoListText": "To-do лист",
+    "todoListText": "Список дел",
     "todoListInfo": "Показать ежедневный список дел",
-    "todoListHover": "To-do лист",
+    "todoListHover": "Список дел",
     "todoPlaceholder": "Добавить задачу...",
 
     // Clock
@@ -37,15 +37,15 @@ const ru = {
     "timeformattitle": "12-часовой формат",
     "timeformatinfo": "Использовать 12-часовой формат времени",
     "greetingtitle": "Приветствие",
-    "greetinginfo": "Показать приветствие под вашим текстом",
+    "greetinginfo": "Показать приветствие под часами",
 
     // Misc
-    "userTextTitle": "Настраиваемый текст",
+    "userTextTitle": "Пользовательский текст",
     "userTextInfo": "Отображение текста под часами",
     "fahrenheitCelsiusCheckbox": "Использовать Фаренгейт",
     "fahrenheitCelsiusText": "Обновите страницу, чтобы применить",
     "hideWeatherTitle": "Скрыть погоду",
-    "hideWeatherInfo": "Отключить виджеты погоды",
+    "hideWeatherInfo": "Отключить виджет погоды",
     "hideWeatherBox": "Скрыть карту погоды",
     "hideWeatherBoxInfo": "Показать только температуру",
     "minMaxTempText": "Мин-Макс температура",
@@ -57,22 +57,22 @@ const ru = {
     "hideSearchWith": "Скрыть поисковые системы",
     "hideSearchWithInfo": "Переключайте поисковые системы, щёлкая по их значку",
     "motivationalQuotesText": "Мотивационные цитаты",
-    "motivationalQuotesInfo": "Показывать мотивационный цитаты под панелью поиска",
+    "motivationalQuotesInfo": "Показывать мотивационные цитаты под панелью поиска",
     "search_suggestions_button": "Поисковые подсказки",
     "search_suggestions_text": "Включить/Отключить поисковые подсказки",
 
     // Proxy
     "useproxytitletext": "Использовать прокси",
     "useproxyText": "Если поисковые подсказки не работают",
-    "ProxyText": "CORS обход прокси",
+    "ProxyText": "CORS-прокси для обхода",
     "ProxySubtext": "Добавьте свой CORS-прокси",
     "HostproxyButton": "Разместить свой прокси",
 
     // Location
-    "useGPS": "Использывать GPS",
+    "useGPS": "Использовать GPS",
     "useGPSInfo": "Включите GPS для обновления местоположения в режиме реального времени",
     "UserLocText": "Введите ваше местоположение",
-    "UserLocSubtext": "Если местоположение для погоды неверно",
+    "UserLocSubtext": "Если местоположение для погоды указано неверно",
     "userLoc": "Ваш город или координаты (Широта, Долгота)",
     "PrivacyPolicy": "Конфиденциальность",
 
@@ -92,10 +92,10 @@ const ru = {
     "bookmarksHeading": "Закладки",
     "bookmarkSortBy": "Сортировка",
     "sortAlphabetical": "A-Z",
-    "sortTimeAdded": "От старых",
+    "sortTimeAdded": "По дате добавления",
     "bookmarkViewAs": "Отображение",
     "bookmarkViewGrid": "Сетка",
-    "bookmarkViewList": "Лист",
+    "bookmarkViewList": "Список",
     "bookmarkSearch": "Поиск закладки",
     "editBookmarkHeading": "Редактировать закладку",
     "editBookmarkName": "Имя закладки",
@@ -111,7 +111,7 @@ const ru = {
     "searchPlaceholder": "Ваш запрос...",
     "listenPlaceholder": "Слушаю...",
     "searchWithHint": "Поиск с",
-    "searchOnHint": "Искать по",
+    "searchOnHint": "Искать в",
     "userText": "Нажмите здесь, чтобы редактировать",
 
     // Greeting
@@ -124,7 +124,7 @@ const ru = {
     // Search Engines
     "defaultEngine": "По умолчанию",
     "googleEngine": "Google",
-    "duckEngine": "Duck",
+    "duckEngine": "DuckDuckGo",
     "bingEngine": "Bing",
     "braveEngine": "Brave",
     "youtubeEngine": "YouTube",
@@ -149,15 +149,15 @@ const ru = {
     "resetAISettingsBtn": "Сбросить",
 
     // Theme
-    "lightThemed": "По умолчанию (Светлая Тема)",
-    "darkThemed": "Чёрная Тема",
-    "systemThemed": "Следовать Системной Теме",
+    "lightThemed": "По умолчанию (Светлая тема)",
+    "darkThemed": "Тёмная тема",
+    "systemThemed": "Следовать системной теме",
 
     // Wallpaper and settings
     "uploadWallpaperText": "Загрузить обои",
     "rangColor": "Выбрать цвет",
-    "opacityTitle": "Прозрачночть",
-    "adjustOpacityDesc": "Настройка прозрачости интерфейса",
+    "opacityTitle": "Прозрачность",
+    "adjustOpacityDesc": "Настройка прозрачности интерфейса",
     "backupText": "Экспорт",
     "restoreText": "Восстановить",
     "resetsettings": "Сброс настроек",
@@ -167,19 +167,19 @@ const ru = {
     "switchSearchModes": "Переключение режимов поиска",
     "switchSearchModesInfo": "Нажмите на ‘Поиск с’, чтобы изменить режим",
     "adjustZoom": "Регулировка масштаба",
-    "adjustZoomInfo": "Нажмите Ctrl + \"+\" или Ctrl + \"-\" чтобы отрегулировать масштаб.",
+    "adjustZoomInfo": "Нажмите Ctrl + \"+\" или Ctrl + \"-\", чтобы отрегулировать масштаб.",
     "changeBrowserTheme": "Изменить тему браузера",
     "chromeThemeInfo": "Нажмите на три точки (⋮) > Дополнительные инструменты > Настройка Chrome, затем выберите цвет, соответствующий теме расширения.",
     "edgeThemeInfo": "Перейдите в Настройки > Внешний вид, затем выберите цвет, соответствующий теме расширения.",
     "braveThemeInfo": 'Посетите <a href="https://chromewebstore.google.com/category/themes/" target="_blank">Chrome Theme Store</a>, загрузите и примените тему, соответствующую расширению.',
     "firefoxThemeInfo": 'Посетите <a href="https://addons.mozilla.org/firefox/themes/" target="_blank">Firefox Themes</a>, загрузите и примените тему, соответствующую расширению.',
     "updateFirefoxHomepage": "Обновить домашнюю страницу",
-    "updateFirefoxHomepageInfo": "Чтобы изменить свою домашнюю страницу на новую вкладку, посетите <a href=\"https://github.com/prem-k-r/MaterialYouNewTab#-firefox\" target=\"_blank\">this link</a> и следуйте инструкциям.",
+    "updateFirefoxHomepageInfo": "Чтобы изменить свою домашнюю страницу на новую вкладку, посетите <a href=\"https://github.com/prem-k-r/MaterialYouNewTab#-firefox\" target=\"_blank\">эту ссылку</a> и следуйте инструкциям.",
     "dontShowTips": "Не показывать снова",
     
     // Footer Toast
-    "footerToastTitle": "Видите Баннер Внизу?",
-    "footerToastMessage": "Чтобы скрыть его, нажмите на него правой кнопкой мыши и выберите 'Скрыть Нижний Колонтитул на Странице Новой Вкладки'.",
+    "footerToastTitle": "Видите баннер внизу?",
+    "footerToastMessage": "Чтобы скрыть его, нажмите на него правой кнопкой мыши и выберите 'Скрыть нижний колонтитул на странице новой вкладки'.",
 
     // Dialog boxes (alerts)
     "okText": "OK",
@@ -190,14 +190,14 @@ const ru = {
     "confirmWallpaper": "Хотите установить новое изображение в качестве обоев на весь день?",
     "confirmRestore": "Вы уверены, что хотите сбросить настройки? Это действие нельзя отменить.",
     "Nobackgroundset": "В настоящее время обои не установлены.",
-    "clearbackgroundimage": "Вы уверены что хотите убрать обои?",
+    "clearbackgroundimage": "Вы уверены, что хотите убрать обои?",
     "ProxyDisclaimer": "По умолчанию все функции прокси отключены.\n\nЕсли вы включите поисковые предложения и прокси для обхода CORS, настоятельно рекомендуется разместить собственный прокси для повышения конфиденциальности.\n\nПо умолчанию прокси будет установлен на https://mynt-proxy.rhythmcorehq.com, то есть все ваши данные будут проходить через этот сервис, что может вызвать проблемы с конфиденциальностью.",
-    "GPSDisclaimer": "Данные о вашем местоположении используются исключительно для предоставления точных обновлений погоды. Они временно хранятся в браузере, не передаются сторонним службам и не доступны нам.\n\n Включая GPS, вы соглашаетесь на передачу данных о вашем местоположении расширению на месте.",
+    "GPSDisclaimer": "Данные о вашем местоположении используются исключительно для предоставления точных обновлений погоды. Они временно хранятся в браузере, не передаются сторонним службам и не доступны нам.\n\nВключая GPS, вы соглашаетесь на передачу данных о вашем местоположении расширению на месте.",
     "failedbackup": "Резервное копирование не удалось: ",
     "restorecompleted": "Восстановление настроек успешно завершено!",
     "restorefailed": "Восстановление настроек не удалось: ",
     "invalidBackup": "Недопустимый файл резервной копии.",
     "deleteBookmark": "Вы уверены, что хотите удалить эту закладку \"{title}\"?",
     "UnsupportedBrowser": "Закладки не поддерживаются в вашем браузере.",
-    "resetShortcutsPrompt": "Все сохраненные ярлыки будут удалены, и их значение будет восстановлено по умолчанию. Хотите продолжить?"
+    "resetShortcutsPrompt": "Все сохранённые ярлыки будут удалены, и их значение будет восстановлено по умолчанию. Хотите продолжить?"
 };
