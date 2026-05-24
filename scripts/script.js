b3a3b1ae6ae73c8cb69bdcc363f12fba472c4777
Prem Kumar <prem12321kumar@gmail.com>
2026-05-24 23:11:11 +0530

    style: Animate hide tips

--- scripts/script.js
+++ scripts/script.js
@@ -47,12 +47,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Check if the user has previously disabled tips
     if (localStorage.getItem("hideTips") === "true") {
-        tips.style.display = "none";
+        tips.classList.add("tips-hidden");
     }
 
     // Hide tips and save preference when button is clicked
     dontShowButton.addEventListener("click", function () {
-        tips.style.display = "none";
+        tips.classList.add("tips-hidden");
         localStorage.setItem("hideTips", "true"); // Save preference
     });
 });
